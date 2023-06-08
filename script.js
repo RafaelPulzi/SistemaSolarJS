01f42abda5719dbab09f96b62719ced5009ce4f3
RafaelPulzi <rafaelpulzi.profissional@gmail.com>
2023-06-08 02:28:47 -0300

    Adicionando orbitas
    No momento adicionei os planetas Terra (junto da lua), Mercúrio e Venus, no caso ainda vou diminuir o tamanho deles para caber os restantes, no caso é apenas questão de tempo

--- script.js
+++ script.js
@@ -3,23 +3,108 @@ function universo() {
     background.style.backgroundColor = '#000000';
 }
 
-function terra() {
-    var canvas = document.createElement('canvas');
-    canvas.id = 'terra';
-  
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-  
-      var circle = new Path2D();
-      circle.moveTo(125, 35);
-      circle.arc(100, 35, 25, 0, 2 * Math.PI);
-  
-      ctx.fillStyle = 'rgb(135, 206, 250)';
-      ctx.fill(circle);
-    }
-  
-    document.body.appendChild(canvas);
+
+universo();
+
+function resizeCanvas() {
+    var canvas = document.getElementById('canvas');
+    canvas.style.position = 'absolute';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    canvas.style.width = window.innerWidth + 'px';
+    canvas.style.height = window.innerHeight + 'px';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
   }
   
-universo();
-terra();
+  window.addEventListener('resize', resizeCanvas);
+  
+  // Inicialização
+  resizeCanvas();
+
+function animate() {
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var centerXSOL = canvas.width / 2; // Coordenada x do centro do canvas
+  var centerYSOL = canvas.height / 2; // Coordenada y do centro do canvas
+
+  //Definição das Orbitas de cada planeta, eles estão na exata ordem para serem adicionados
+  var angleMercurio = 0;
+  var angleVenus = 0;
+  var angleTerra = 0;
+  var angleLua = 0;
+  var angleMarte = 0;
+  var angleJupiter = 0;
+  var angleSaturno = 0;
+  var angleNetuno = 0;
+  var anglePlutão = 0;  
+
+  function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // SOL
+    context.beginPath();
+    context.arc(centerXSOL, centerYSOL, 100, 0, 2 * Math.PI, false);
+    context.fillStyle = 'rgb(254, 169, 26)';
+    context.fill();
+
+    // Mercurio
+    // Calculo da Mercurio em relação ao sol
+    var radiusMercurio = 30; // Raio de Mercurio
+    var xMercurio = centerXSOL + Math.cos(angleMercurio) * 150; // Distancia referente ao sol
+    var yMercurio = centerYSOL + Math.sin(angleMercurio) * 150;
+
+    context.beginPath();
+    context.arc(xMercurio, yMercurio, radiusMercurio, 0, 2 * Math.PI, false);
+    context.fillStyle = 'rgb(204, 132, 0)';
+    context.fill();
+
+    angleMercurio += 0.01;
+
+    // Venus
+    // Calculo da Venus em relação ao sol
+    var radiusVenus = 33; // Raio de Venus
+    var xVenus = centerXSOL + Math.cos(angleVenus) * 250; // Distancia referente ao sol
+    var yVenus = centerYSOL + Math.sin(angleVenus) * 250;
+
+    context.beginPath();
+    context.arc(xVenus, yVenus, radiusVenus, 0, 2 * Math.PI, false);
+    context.fillStyle = 'rgb(246, 230, 231)';
+    context.fill();
+
+    angleVenus += 0.004;
+    
+    // Terra
+    // Calculo da terra em relação ao sol
+    var radiusTerra = 45; // Raio da terra
+    var xTerra = centerXSOL + Math.cos(angleTerra) * 400; // Distancia referente ao sol
+    var yTerra = centerYSOL + Math.sin(angleTerra) * 400;
+
+    context.beginPath();
+    context.arc(xTerra, yTerra, radiusTerra, 0, 2 * Math.PI, false);
+    context.fillStyle = 'rgb(135, 206, 250)';
+    context.fill();
+
+    angleTerra += 0.001;
+
+    // LUA
+    // Calculo da posição da lua em relação a terra
+    var radiusLua = 25; 
+    var xLua = xTerra + Math.cos(angleLua) * 100; 
+    var yLua = yTerra + Math.sin(angleLua) * 100;
+
+    
+    context.beginPath();
+    context.arc(xLua, yLua, radiusLua, 0, 2 * Math.PI, false);
+    context.fillStyle = 'rgb(216, 212, 202)';
+    context.fill();
+
+    angleLua += 0.007; // velocidade de rotação da lua
+
+    requestAnimationFrame(draw);
+  }
+
+  draw();
+}
+
+animate();
