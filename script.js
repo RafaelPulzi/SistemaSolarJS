4d6a4dca574fdb0673eece12970b4c6ce7ab0e6b
RafaelPulzi <rafaelpulzi.profissional@gmail.com>
2023-07-01 15:16:14 -0300

    Começo do efeito long shadow

--- script.js
+++ script.js
@@ -31,7 +31,21 @@ function styleScrollbar() {
 
 // Chame a função para estilizar a barra de rolagem
 styleScrollbar();
-
+/*
+function drawLight() {
+  ctx.beginPath();
+  ctx.arc(light.x, light.y, 1000, 0, 2 * Math.PI);
+  var gradient = ctx.createRadialGradient(
+    light.x,
+    light.y,
+    0,
+    light.x,
+    light.y,
+    1000
+  );
+  gradient.addColorStop(0, "#3b4654");
+  gradient.addColorStop(1, "#2c343f");
+*/
 function resizeCanvas() {
   var canvas = document.getElementById('canvas');
   canvas.style.position = 'absolute';
